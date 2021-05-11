ef692343f005530e7ce994059f01ff5011253b06
thalesallan <trabalhecomthales@gmail.com>
2021-05-11 10:04:34 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1(Classes, Atributos e métodos)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo R = new Retangulo();
+            Console.WriteLine("Entre com a Largura, Altura do retângulo: ");
+
+            Console.WriteLine("--------------------------");
+            Console.Write("Largura: ");
+            double Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("--------------------------");
+            Console.Write("Altura: ");
+            double Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            R.CalcularRetangulo(Largura, Altura);
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(R);
+        }
+    }
+}
---
+++ Exercicio1(Classes, Atributos e métodos)/Retangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio1
+{
+    class Retangulo
+    {
+        public double Area;
+        public double Perimetro;
+        public double Diagonal;
+
+        public void CalcularRetangulo(double largura, double altura)
+        {
+            Area = largura * altura;
+            Perimetro = 2 * (largura + altura);
+            Diagonal = Math.Sqrt((largura * largura) + (altura * altura));
+        }
+
+        public override string ToString()
+        {
+            return "Area = " 
+                + Area
+                + " \nPerímetro = " 
+                + Perimetro 
+                + " \nDiagonal = " 
+                + Diagonal;
+             
+            
+        }
+    }
+
+    
+}
